f5b94b96f73968c954c5c2f4e6747079a4828648
geoffsugden <geoffsugden@gmail.com>
2026-04-03 17:32:11 +1300

    Completed exercises up to and including Part 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,22 +1,62 @@
-const Hello = (props) => {
+import { Component } from "react"
+
+const Header = (props) => {
   return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+      <div id='Content'>
+        <Part part={props.course.parts[0]} />  
+        <Part part={props.course.parts[1]} />  
+        <Part part={props.course.parts[2]} />  
+      </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part.name} {props.part.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  let tot = 0
+  props.course.parts.forEach(value => {
+    tot += value.exercises
+  })
+  return (
+    <p>Number of exercises {tot}</p>
   )
 }
 
 const App = () => {
-  const name ='Peter'
-  const age = 10
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { 
+      name:'Fundamentals of React',
+      exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
+
+export default App
