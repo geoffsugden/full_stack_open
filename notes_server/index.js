d89baf7882fadc27c11a630db0449f8fb0ea5cba
geoffsugden <geoffsugden@gmail.com>
2026-06-02 20:55:08 +1200

    fixed minor naming error

--- notes_server/index.js
+++ notes_server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config
 const express = require('express')
-const mongoose = require('mongoose')
 const Note = require('./models/note')
+
 const app = express()
 
 app.use(express.json())
@@ -11,29 +11,6 @@ if(process.argv.length < 3) {
     console.log('Please enter a password for the database.')
     process.exit(1)
 }
-
-const db_user = 'geoffsugden_db_user'
-const db_password = process.argv[2]
-const db_name = 'noteApp'
-const url = `mongodb://${db_user}:${db_password}@ac-koqzjed-shard-00-00.fwcdt3n.mongodb.net:27017,ac-koqzjed-shard-00-01.fwcdt3n.mongodb.net:27017,ac-koqzjed-shard-00-02.fwcdt3n.mongodb.net:27017/${db_name}?ssl=true&replicaSet=atlas-12ibee-shard-0&authSource=admin&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url, {family : 4})
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Note = mongoose.model('Note', noteSchema)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -91,7 +68,7 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
