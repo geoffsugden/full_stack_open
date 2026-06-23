93307dcdf688c8399d709d21008c7f790f8d5e70
geoffsugden <geoffsugden@gmail.com>
2026-06-23 21:26:55 +1200

    feat: Add token expiration error handling in middleware

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -30,7 +30,7 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if(!decodedToken.id) { 
+  if(!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
   const user = await User.findById(decodedToken.id)
