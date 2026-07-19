cdf6301e062a6d7589a53d4d11cbc8576774b3a3
geoffsugden <geoffsugden@gmail.com>
2026-07-19 12:31:24 +1200

    [feat]: Add small example counter application and base unicafe application to work from (unicafe-zustand from University of Helsinki)

---
+++ part6/counter/src/App.jsx
@@ -0,0 +1,14 @@
+import Controls from './Controls'
+import Display from './Display'
+
+const App = () => {
+
+  return ( 
+    <div>
+      <Display />
+      <Controls />
+    </div>
+  )
+}
+
+export default App
