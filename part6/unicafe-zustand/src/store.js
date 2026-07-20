ca827ff3e8c93ccd6b42ba487bd730c96ee43eb2
geoffsugden <geoffsugden@gmail.com>
2026-07-20 20:32:31 +1200

    [feat]: add zustand notes from course provided repo. Seperate to logical components and extend store

--- part6/unicafe-zustand/src/store.js
+++ part6/unicafe-zustand/src/store.js
@@ -5,9 +5,11 @@ const useFeedbackStore = create(set => ({
   good: 0, 
   neutral: 0, 
   bad: 0,
-  giveGood: () => (set(state => ({ good: state.good + 1}))),
-  giveNeutral: () => (set(state => ({ neutral: state.neutral + 1}))),
-  giveBad: () => (set(state => ({ bad: state.bad + 1}))),
+  actions: {
+    giveGood: () => (set(state => ({ good: state.good + 1}))),
+    giveNeutral: () => (set(state => ({ neutral: state.neutral + 1}))),
+    giveBad: () => (set(state => ({ bad: state.bad + 1}))),
+  }
 }))
 
 export const useFeedback = () => useFeedbackStore(useShallow(state => ({ 
@@ -15,8 +17,4 @@ export const useFeedback = () => useFeedbackStore(useShallow(state => ({
   neutral: state.neutral, 
   bad: state.bad,
 })))
-export const useFeedbackControls = () => useFeedbackStore(useShallow(state => ({ 
-  giveGood: state.giveGood, 
-  giveNeutral: state.giveNeutral, 
-  giveBad: state.giveBad,
-})))
+export const useFeedbackControls = () => useFeedbackStore((state => state.actions))
---
+++ zustand-notes/src/App.jsx
@@ -0,0 +1,10 @@
+import NoteForm from './components/NoteForm'
+import NoteList from './components/NoteList'
+
+const App = () => (
+  <div>
+    <NoteForm />
+    <NoteList />
+  </div>
+)
+export default App
