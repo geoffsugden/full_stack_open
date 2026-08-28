641e4c38cd90a458c9f819369ee63bd61f14c496
geoffsugden <geoffsugden@gmail.com>
2026-08-28 14:48:39 +1200

    [feat]: replace inline comment schema with a separate commentSchema for improved structure, readability and front end ease of use.

--- part7/blogapp-query/server/models/blog.js
+++ part7/blogapp-query/server/models/blog.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose')
 
+const commentSchema = mongoose.Schema({ comment: { type: String } })
+
+commentSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
 const blogSchema = mongoose.Schema({
   title: { type: String, required: true },
   author: String,
   url: { type: String, required: true },
   likes: { type: Number, default: 0 },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  comments: [{ comment: { type: String } }],
+  comments: [commentSchema],
 })
 
 blogSchema.set('toJSON', {
